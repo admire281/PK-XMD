eaee4fa94f7a787a658a60dae339e703c3893115
admire281 <admirechishawa7@gmail.com>
2025-08-14 16:32:53 +0200

    Update config.js
    Likees

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "pk~9NIyTD4I#hXSW5cbV9t9Pk7Zu6WNG4Phj4ahPuS25Rzz7NmNaC-w",
+SESSION_ID: process.env.SESSION_ID || "pk~b4pxFb4L#Jz6Sg61JtxLERzmZJCN-tBi9AUSj4DIvW2Blkogj6lU",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
